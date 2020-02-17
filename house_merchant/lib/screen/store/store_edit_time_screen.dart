e3b002fdc570869aa6cfb001fbc612cd46ff4ca3
Minh Ngoc <ngocntm@housemap.vn>
2020-02-17 09:43:40 +0700

    fix UI màn hình Chỉnh sửa cửa hàng

--- house_merchant/lib/screen/store/store_edit_time_screen.dart
+++ house_merchant/lib/screen/store/store_edit_time_screen.dart
@@ -31,7 +31,8 @@ class StoreEditTimeScreenState extends State<StoreEditTimeScreen> {
       new StreamController<ButtonSubmitEvent>.broadcast();
   ProgressHUD _progressToolkit = Progress.instanceCreate();
 
-  StreamController<String> messageText = new StreamController<String>.broadcast();
+  StreamController<String> messageText =
+      new StreamController<String>.broadcast();
   final _fOpeningHours = DropdownWidgetController();
   final _fCloseHours = DropdownWidgetController();
   final _dataSourceHours = [];
@@ -53,7 +54,8 @@ class StoreEditTimeScreenState extends State<StoreEditTimeScreen> {
   void initState() {
     super.initState();
     this._shopModel = widget.params['shop_model'] as ShopModel;
-    this._selectedWorkingDayList = this._shopModel.hours.map((f)=>f.weekday).toList();
+    this._selectedWorkingDayList =
+        this._shopModel.hours.map((f) => f.weekday).toList();
     //Init dataSourceYear
     var times = [
       "00:00",
@@ -112,8 +114,10 @@ class StoreEditTimeScreenState extends State<StoreEditTimeScreen> {
     _initOpeningHourIndex = _dataSourceHours.first.key;
     _initCloseHourIndex = _dataSourceHours.last.key;
     if (this._shopModel.hours != null && this._shopModel.hours.length > 0) {
-      _initOpeningHourIndex = times.indexWhere((time) => time == this._shopModel.hours[0].startTime);
-      _initCloseHourIndex = times.indexWhere((time) => time == this._shopModel.hours[0].endTime);
+      _initOpeningHourIndex = times
+          .indexWhere((time) => time == this._shopModel.hours[0].startTime);
+      _initCloseHourIndex =
+          times.indexWhere((time) => time == this._shopModel.hours[0].endTime);
     }
 
     var workingDays = ["T2", "T3", "T4", "T5", "T6", "T7", "CN"];
@@ -178,26 +182,31 @@ class StoreEditTimeScreenState extends State<StoreEditTimeScreen> {
     }
 
     Widget workingDaySection() {
-
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ChoiceChipsWidget(
-            _dataSourceWorkingDay,
-            selectedChoices: _selectedWorkingDayList,
-            onSelectionChanged: (selectedList) {
-              _selectedWorkingDayList = selectedList;
-              this.checkValidation();
-              this._shopModel.hours = _selectedWorkingDayList.map((f) => Hours(
-                startTime: openTime,
-                endTime: closeTime,
-                weekday: f,
-              )).toList();
-            },
-          ),
-        ],
-      );
+      return Container(
+          height: 50.0,
+          child: ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ChoiceChipsWidget(
+                  _dataSourceWorkingDay,
+                  selectedChoices: _selectedWorkingDayList,
+                  onSelectionChanged: (selectedList) {
+                    _selectedWorkingDayList = selectedList;
+                    this.checkValidation();
+                    this._shopModel.hours = _selectedWorkingDayList
+                        .map((f) => Hours(
+                              startTime: openTime,
+                              endTime: closeTime,
+                              weekday: f,
+                            ))
+                        .toList();
+                  },
+                ),
+              ],
+            )
+          ]));
     }
 
     //TODO
@@ -234,153 +243,146 @@ class StoreEditTimeScreenState extends State<StoreEditTimeScreen> {
 //    }
 
     return Container(
-      decoration: BoxDecoration(color: ThemeConstant.white_color),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            'Thời gian',
-            style: ThemeConstant.titleLargeStyle(Colors.black),
-          ),
-          SizedBox(height: 30),
-          _titleSection('Chọn ngày làm việc'),
-          SizedBox(height: 10),
-          workingDaySection(),
-          SizedBox(height: 30),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: ((_screenSize.width - (_padding * 2)) / 2) - 10,
-                  child: Container(
-                    child: Column(
+        decoration: BoxDecoration(color: ThemeConstant.white_color),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Thời gian',
+                style: ThemeConstant.titleLargeStyle(Colors.black),
+              ),
+              SizedBox(height: 30),
+              _titleSection('Chọn ngày làm việc'),
+              SizedBox(height: 10),
+              workingDaySection(),
+              SizedBox(height: 30),
+              Container(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        _titleSection("Giờ mở cửa"),
-                        SizedBox(height: 10),
-                        DropdownWidget(
-                          controller: _fOpeningHours,
-                          defaultHintText: LocalizationsUtil.of(context)
-                              .translate('Chọn giờ'),
-                          dataSource: _dataSourceHours,
-                          centerText: true,
-                          buildChild: (index) {
-                            return Center(
-                              child: Text(
-                                "${_dataSourceHours[index].value}",
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                    letterSpacing:
-                                    ThemeConstant.letter_spacing_026,
-                                    fontWeight: FontWeight.w500),
-                              ));
-                          },
-                          initIndex: _initOpeningHourIndex,
-                          doneEvent: (index) async {
-                            _initOpeningHourIndex = index;
-                            this.checkValidation();
-                            //print(_dataSourceHours[index].key);
-                            openTime = _dataSourceHours[index].value;
-                          })
-                      ]
-                    )
-                  )
-                ),
-
-                Container(
-                  width: ((_screenSize.width - (_padding * 2)) / 2) - 10,
-                  child: Container(
-                    child: Column(
-                      children: <Widget>[
-                        _titleSection("Giờ đóng cửa"),
-                        SizedBox(height: 10),
-                        DropdownWidget(
-                            controller: _fCloseHours,
-                            defaultHintText: LocalizationsUtil.of(context)
-                                .translate('Chọn giờ'),
-                            dataSource: _dataSourceHours,
-                            centerText: true,
-                            buildChild: (index) {
-                              return Center(
-                                  child: Text(
+                    Container(
+                        width: ((_screenSize.width - (_padding * 2)) / 2) - 10,
+                        child: Container(
+                            child: Column(children: <Widget>[
+                          _titleSection("Giờ mở cửa"),
+                          SizedBox(height: 10),
+                          DropdownWidget(
+                              controller: _fOpeningHours,
+                              defaultHintText: LocalizationsUtil.of(context)
+                                  .translate('Chọn giờ'),
+                              dataSource: _dataSourceHours,
+                              centerText: true,
+                              buildChild: (index) {
+                                return Center(
+                                    child: Text(
+                                  "${_dataSourceHours[index].value}",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      letterSpacing:
+                                          ThemeConstant.letter_spacing_026,
+                                      fontWeight: FontWeight.w500),
+                                ));
+                              },
+                              initIndex: _initOpeningHourIndex,
+                              doneEvent: (index) async {
+                                _initOpeningHourIndex = index;
+                                this.checkValidation();
+                                //print(_dataSourceHours[index].key);
+                                openTime = _dataSourceHours[index].value;
+                              })
+                        ]))),
+                    Container(
+                      width: ((_screenSize.width - (_padding * 2)) / 2) - 10,
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            _titleSection("Giờ đóng cửa"),
+                            SizedBox(height: 10),
+                            DropdownWidget(
+                                controller: _fCloseHours,
+                                defaultHintText: LocalizationsUtil.of(context)
+                                    .translate('Chọn giờ'),
+                                dataSource: _dataSourceHours,
+                                centerText: true,
+                                buildChild: (index) {
+                                  return Center(
+                                      child: Text(
                                     "${_dataSourceHours[index].value}",
                                     style: TextStyle(
                                         fontSize: 20,
                                         color: Colors.black,
                                         letterSpacing:
-                                        ThemeConstant.letter_spacing_026,
+                                            ThemeConstant.letter_spacing_026,
                                         fontWeight: FontWeight.w500),
                                   ));
-                            },
-                            initIndex: _initCloseHourIndex,
-                            doneEvent: (index) async {
-                              _initCloseHourIndex = index;
-                              this.checkValidation();
-                              //print(_dataSourceHours[index].value);
-                              closeTime = _dataSourceHours[index].value;
-                            })
-                      ],
-                    ),
-                  ),
-                )
-              ]
-            )
-          ),
-          SizedBox(height: 20),
-          TextWidget('',
-            controller: messageText,
-            style:
-            ThemeConstant.subtitleStyle(ThemeConstant.required_color),
-          ),
-        ]));
+                                },
+                                initIndex: _initCloseHourIndex,
+                                doneEvent: (index) async {
+                                  _initCloseHourIndex = index;
+                                  this.checkValidation();
+                                  //print(_dataSourceHours[index].value);
+                                  closeTime = _dataSourceHours[index].value;
+                                })
+                          ],
+                        ),
+                      ),
+                    )
+                  ])),
+              SizedBox(height: 20),
+              TextWidget(
+                '',
+                controller: messageText,
+                style:
+                    ThemeConstant.subtitleStyle(ThemeConstant.required_color),
+              ),
+            ]));
   }
 
   Widget saveChangeButton() {
     return ButtonWidget(
-      controller: saveButtonController,
-      defaultHintText:
-      LocalizationsUtil.of(_context).translate('Lưu thay đổi'),
-      callback: () async {
-        if (this.checkValidation()) {
-          _progressToolkit.state.show();
-          try {
+        controller: saveButtonController,
+        defaultHintText:
+            LocalizationsUtil.of(_context).translate('Lưu thay đổi'),
+        callback: () async {
+          if (this.checkValidation()) {
+            _progressToolkit.state.show();
+            try {
+              //Reupdate hours
+              this._shopModel.hours = _selectedWorkingDayList
+                  .map((f) => Hours(
+                        startTime: openTime,
+                        endTime: closeTime,
+                        weekday: f,
+                      ))
+                  .toList();
 
-            //Reupdate hours
-            this._shopModel.hours = _selectedWorkingDayList.map((f) => Hours(
-              startTime: openTime,
-              endTime: closeTime,
-              weekday: f,
-            )).toList();
-
-            final result = shopRepository.updateInfo(this._shopModel);
-            Navigator.of(context).pop();
-            Fluttertoast.showToast(
-              msg: 'Cập nhật thời gian thành công',
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.CENTER,
-              timeInSecForIos: 5,
-              backgroundColor: Colors.black,
-              textColor: Colors.white,
-              fontSize: 14.0
-            );
-          } catch (e) {
-            Fluttertoast.showToast(
-              msg: e.toString(),
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.CENTER,
-              timeInSecForIos: 5,
-              backgroundColor: Colors.black,
-              textColor: Colors.white,
-              fontSize: 14.0
-            );
-          } finally {
-            _progressToolkit.state.dismiss();
+              final result = shopRepository.updateInfo(this._shopModel);
+              Navigator.of(context).pop();
+              Fluttertoast.showToast(
+                  msg: 'Cập nhật thời gian thành công',
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIos: 5,
+                  backgroundColor: Colors.black,
+                  textColor: Colors.white,
+                  fontSize: 14.0);
+            } catch (e) {
+              Fluttertoast.showToast(
+                  msg: e.toString(),
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIos: 5,
+                  backgroundColor: Colors.black,
+                  textColor: Colors.white,
+                  fontSize: 14.0);
+            } finally {
+              _progressToolkit.state.dismiss();
+            }
           }
-        }
-      });
+        });
   }
 
   @override
@@ -391,20 +393,22 @@ class StoreEditTimeScreenState extends State<StoreEditTimeScreen> {
 
     // TODO: implement build
     return BaseScaffoldNormal(
-      title: 'Chỉnh sửa cửa hàng',
-      child: SafeArea(
-        child: Stack(children: <Widget>[
+        title: 'Chỉnh sửa cửa hàng',
+        child: SafeArea(
+            child: Stack(children: <Widget>[
           Container(
             padding: EdgeInsets.all(this._padding),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[Expanded(child: buildBody()), saveChangeButton()],
+              children: <Widget>[
+                Expanded(child: buildBody()),
+                saveChangeButton()
+              ],
             ),
             color: ThemeConstant.background_white_color,
           ),
           _progressToolkit
-        ])
-      ));
+        ])));
   }
 }
