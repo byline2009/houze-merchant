a12122245df1689e360021c1c0aa5cde3f6a8b1d
Dan Hon <dan.honvi@gmail.com>
2020-02-17 13:48:06 +0700

    Fixed crash android

--- house_merchant/android/app/src/main/kotlin/com/house/merchant/MainActivity.kt
+++ house_merchant/android/app/src/main/kotlin/com/house/merchant/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.house.merchant
 
+import android.content.Intent
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
@@ -9,5 +10,9 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
   }
 }
