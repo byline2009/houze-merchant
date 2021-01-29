d995c5117b916438cd5827c80d6b798b277f319a
Minh Ngoc <ngocntm@housemap.vn>
2021-01-29 13:38:48 +0700

    add lib & display full image

---
+++ house_merchant/lib/screen/base/sc_image_view.dart
@@ -0,0 +1,56 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+class ImageViewScreenArgument {
+  final String title;
+  final List<String> images;
+  const ImageViewScreenArgument({@required this.images, this.title});
+}
+
+class ImageViewScreen extends StatelessWidget {
+  final ImageViewScreenArgument params;
+
+  const ImageViewScreen({this.params});
+
+  @override
+  Widget build(BuildContext context) {
+    final title = params.title ?? '';
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.0),
+        child: AppBar(
+          title: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.black,
+          iconTheme: IconThemeData(color: Colors.white),
+          actions: <Widget>[],
+        ),
+      ),
+      body: Container(
+        color: Colors.black,
+        child: PhotoViewGallery.builder(
+          scrollPhysics: const BouncingScrollPhysics(),
+          loadFailedChild: Center(
+            child: Icon(Icons.error_outline, color: Colors.white),
+          ),
+          builder: (BuildContext context, int index) {
+            return PhotoViewGalleryPageOptions(
+              imageProvider:
+                  CachedNetworkImageProvider(params.images.elementAt(index)),
+              initialScale: PhotoViewComputedScale.contained * 0.8,
+            );
+          },
+          itemCount: params.images.length,
+        ),
+      ),
+    );
+  }
+}
